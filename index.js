a7e35677a3878250b7210d589817f31c32fcda39
Joshua Vance <skinsfandc0612@icloud.com>
2019-02-20 15:29:41 -0500

    began building out but a little lost and confused

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const handlebars = require("hbs");
+const bodyParser = require("body-Parser");
+
+app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(require("./routes/index.js"));
 
 app.listen(3009, () => console.log("listening on port 3009"));
