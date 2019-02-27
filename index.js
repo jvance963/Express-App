2e193288ccd2aa544d46224acd213e1dc94eb5a2
Joshua Vance <skinsfandc0612@icloud.com>
2019-02-27 16:51:59 -0500

    linked index to routes

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
-// // this makes express available
-
-//
-
 const app = express();
+const parser = require("body-parser");
+const methodOverride = require("method-override");
+
 app.set("view engine", "hbs");
+app.set("view engine", "hbs");
+app.use(parser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // // creates the variable "app" global to get methods
-
-app.listen(4000, () => {
-  console.log("app listening on port 4000");
-});
 
 // app.get("/", (request, response) => {
 //   response.render("Hello World");
@@ -19,5 +17,11 @@ app.listen(4000, () => {
 // Getting URL from the homepage and request the response and then send the response "Hello World"
 
 app.get("/", (req, res) => {
-  res.render("index", { name: req.params.name });
+  res.render("index");
 });
+
+app.use("./routes/index", (req, res) => {
+  res.render("index");
+});
+
+app.listen(4000, () => console.log("listening on port 4000"));
