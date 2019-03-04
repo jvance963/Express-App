5c0639f0e36e82d2679d4e22fa4c68f8a8e0b50a
Joshua Vance <skinsfandc0612@icloud.com>
2019-03-03 23:58:07 -0500

    I needed to go in and adjust styling of the application as this turned out to be a key issue

--- model/post.js
+++ model/post.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const ResponseSchema = require("./response");
+
+const ResponseSchema = new Schema({
+  content: String,
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
 
 const PostSchema = new Schema({
   name: String,
   description: String,
   status: {
     type: String
-    // want to make this a checkmark in a box if I can
   },
   priority: {
     type: Number,
